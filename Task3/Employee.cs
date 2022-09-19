fe431c6451d145ce63c56b3200c4f0a98d923b03
Joshimore <egorladur@gmail.com>
2022-09-19 13:51:33 +0300

    1st commit (test)

---
+++ Task3/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Employee
+    {
+        private string name;
+        private string surname;
+        private string post;
+        private int salary;
+        private int experience;
+        private int taxe;
+
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+        public string Surname
+        {
+            set
+            {
+                surname= value;
+            }
+            get
+            {
+                return surname;
+            }
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            Console.WriteLine("                                           ****GLOBAL TRADE&co****" +
+                "\n\nIt is an employee catalog. \n For more info about specific person type a full name and surname below:");
+        }
+    }
+}
