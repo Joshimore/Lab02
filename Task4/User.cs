b495b61dc2ad88cbf494881734527e4a7418f880
Joshimore <egorladur@gmail.com>
2022-09-25 20:38:13 +0300

    Task4 changes

--- Task4/User.cs
+++ Task4/User.cs
@@ -8,5 +8,27 @@ namespace Task4
 {
     internal class User
     {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public DateTime CreationTime { get; set; }
+
+        public Usser(string login, string name, string surname, int age, DateTime creationtime)
+        {
+            this.Login = login;
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+            this.CreationTime = creationtime;
+        }
+
+        public void usrinfo()
+        {
+            Console.WriteLine("Name: ", Name, Surname);
+            Console.WriteLine("Age: ", Age);
+            Console.WriteLine("Login: ", Login);
+            Console.WriteLine("Creation time: ", CreationTime);
+        }
     }
 }
